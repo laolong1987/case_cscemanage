c440f72a266fcb7ff5225a7aeb2c0e092416a1fb
sukeyYang <yangshukai@haistand.com.cn>
2016-03-13 15:24:35 +0800

    modify userDao

--- src/main/java/com/web/dao/UserDao.java
+++ src/main/java/com/web/dao/UserDao.java
@@ -2,7 +2,10 @@ package com.web.dao;
 
 
 import com.common.BaseDao;
+import com.web.entity.User;
 
 public class UserDao extends BaseDao{
-
+    public User findUser(User user){
+        return user;
+    }
 }
